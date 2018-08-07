1f090f3a5106f2d26a7ed11fda4863ac9298b9f3
viswanadham <sreenatha.kk@gmail.com>
2018-08-07 17:48:57 +0530

    modified files

---
+++ testing/Scripts/RemoveRG.ps1
@@ -0,0 +1,50 @@
+﻿Param(
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    [string] $SubscriptionId,
+    [Parameter(Mandatory=$True)]
+    [String] $Username,
+    [Parameter(Mandatory=$True)]
+    [string] $Password,
+     [Parameter(Mandatory=$True)]
+    [string] $resourceGroupName
+ 
+)
+<#
+$t = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
+add-type -name win -member $t -namespace native
+[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
+#>
+
+                    do{
+                        
+                        if (!(Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue -ListAvailable)) 
+                        {
+                        Install-PackageProvider NuGet -Force | Out-Null
+                        }
+                        
+                        $LoadModule=Get-Module -ListAvailable "Azure*"
+                        
+                        if(!$LoadModule){
+                        Install-Module -Name AzureRM.profile -AllowClobber -Force | Out-Null
+                        Install-Module -Name AzureRM.resources -AllowClobber -Force | Out-Null
+                        Install-Module -Name AzureRM.Compute -AllowClobber -Force | Out-Null
+                        }
+                        } until($LoadModule)
+
+Import-Module AzureRM.profile
+Import-Module AzureRM.resources
+Import-Module AzureRM.Compute
+
+        try{
+        $Securepass=ConvertTo-SecureString -String $Password -AsPlainText -Force
+        $Azurecred=New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList($Username, $Securepass)
+        $login=Login-AzureRmAccount -Credential $Azurecred -SubscriptionId $SubscriptionId
+        $getRGInfo=Get-AzureRmResourceGroup -Name $resourceGroupName
+        $test=New-AzureRmAvailabilitySet -ResourceGroupName $resourceGroupName -Name "Test-avset" -Location $getRGInfo.Location
+        Remove-AzureRmResourceGroup -Name $resourceGroupName -Force
+                       
+        }
+        catch{
+        Write-Error $_.Exception.Message
+        }
--- testing/Scripts/Script.ps1
+++ testing/Scripts/Script.ps1
@@ -35,9 +35,9 @@ New-Item -Path "C:\PSModules" -ItemType directory -Force -ErrorAction SilentlyCo
 Expand-Archive "C:\PSModules.zip" -DestinationPath "C:\PSModules" -ErrorAction SilentlyContinue
 Set-Location "C:\PSModules"
 
-Start-Job -ScriptBlock{
+Invoke-Command -ComputerName localhost -ScriptBlock{
 param($SubscriptionId,$Username,$Password,$resourceGroupName)
 
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'C:\PSModules\RemoveRG.ps1' -SubscriptionId $SubscriptionId -Username $Username -Password $Password -resourceGroupName $resourceGroupName"
 
-} -ArgumentList($SubscriptionId,$Username,$Password,$resourceGroupName)
+} -ArgumentList($SubscriptionId,$Username,$Password,$resourceGroupName) -AsJob
