6ccba0196c2ed9f37fd3af9381978a1479f23b19
viswanadham <sreenatha.kk@gmail.com>
2018-12-26 18:30:37 +0530

    modified files

--- sysprep.ps1
+++ sysprep.ps1
@@ -3,7 +3,7 @@
 
 
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
-function office365Installation{
+
 $url = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_11107-33602.exe"
 $output = "D:/odt.exe"
 Import-Module BitsTransfer
@@ -14,11 +14,7 @@ Set-Location "D:\O365"
 Invoke-Expression -Command "cmd.exe /c '.\setup.exe' /download 'D:\O365\configuration-Office365-x64.xml'" 
 Invoke-Expression -Command "cmd.exe /c 'D:\O365\setup.exe' /configure 'D:\O365\configuration-Office365-x64.xml'" 
 
-}
 
-office365Installation
-
-function RunSysprep{
 if($runSysprep){
   write-output "starting the Sysprep"
   Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/generalize /oobe /shutdown /quiet'
@@ -26,6 +22,4 @@ if($runSysprep){
 }else{
   write-output "skipping the Sysprep"
 }
-}
 
-RunSysprep
