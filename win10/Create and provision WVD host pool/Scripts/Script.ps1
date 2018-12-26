05079a912522d1586e592865fdb9cbcec09ad8c3
viswanadham <sreenatha.kk@gmail.com>
2018-12-26 19:07:34 +0530

    added files

--- win10/Create and provision WVD host pool/Scripts/Script.ps1
+++ win10/Create and provision WVD host pool/Scripts/Script.ps1
@@ -89,6 +89,13 @@ function Write-Log
     } 
 }
 
+Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope Process -Force -Confirm:$false
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force -Confirm:$false
+$PolicyList=Get-ExecutionPolicy -List
+$log = $PolicyList | Out-String 
+Write-Log -Message "Policy List: $log" 
+
+
 function ActivateWvdSku
 {
     param
