95461c6b57d7cab32d0edae8fba60ae2e1cbbf07
viswanadham <sreenatha.kk@gmail.com>
2018-12-27 14:14:23 +0530

    added a file

---
+++ Create and provision new RDmi hostpool/DSC/Configuration/Configuration.ps1
@@ -0,0 +1,133 @@
+configuration DomainJoin 
+{ 
+   param 
+    ( 
+        [Parameter(Mandatory)]
+        [String]$domainName,
+
+        [Parameter(Mandatory)]
+        [PSCredential]$adminCreds,
+
+        [Int]$RetryCount=200,
+
+        [Int]$RetryIntervalSec=30
+    ) 
+    
+    Import-DscResource -ModuleName xActiveDirectory, xComputerManagement, xNetworking
+
+    $domainCreds = New-Object System.Management.Automation.PSCredential ("$domainName\$($adminCreds.UserName)", $adminCreds.Password)
+   
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+        WindowsFeature ADPowershell
+        {
+            Name = "RSAT-AD-PowerShell"
+            Ensure = "Present"
+        } 
+
+        xWaitForADDomain DscForestWait 
+        { 
+            DomainName = $domainName 
+            DomainUserCredential= $domainCreds
+            RetryCount = $RetryCount 
+            RetryIntervalSec = $RetryIntervalSec 
+            DependsOn = "[WindowsFeature]ADPowershell" 
+        }
+
+        xComputer DomainJoin
+        {
+            Name = $env:COMPUTERNAME
+            DomainName = $domainName
+            Credential = $domainCreds
+            DependsOn = "[xWaitForADDomain]DscForestWait" 
+        } 
+        Registry RdmsEnableUILog
+        {
+            Ensure = "Present"
+            Key = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\RDMS"
+            ValueName = "EnableUILog"
+            ValueType = "Dword"
+            ValueData = "1"
+        }
+
+        Registry EnableDeploymentUILog
+        {
+            Ensure = "Present"
+            Key = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\RDMS"
+            ValueName = "EnableDeploymentUILog"
+            ValueType = "Dword"
+            ValueData = "1"
+        }
+
+        Registry EnableTraceLog
+        {
+            Ensure = "Present"
+            Key = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\RDMS"
+            ValueName = "EnableTraceLog"
+            ValueType = "Dword"
+            ValueData = "1"
+        }
+
+        Registry EnableTraceToFile
+        {
+            Ensure = "Present"
+            Key = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\RDMS"
+            ValueName = "EnableTraceToFile"
+            ValueType = "Dword"
+            ValueData = "1"
+        }
+
+   }
+}
+
+
+configuration SessionHost
+{
+   param 
+    ( 
+        [Parameter(Mandatory)]
+        [String]$domainName,
+
+        [Parameter(Mandatory)]
+        [PSCredential]$adminCreds
+    ) 
+
+Import-DscResource -ModuleName xActiveDirectory, xComputerManagement, xNetworking
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            RebootNodeIfNeeded = $true
+            ConfigurationMode = "ApplyOnly"
+        }
+
+        DomainJoin DomainJoin
+        {
+            domainName = $domainName 
+            adminCreds = $adminCreds 
+        }
+        xFirewall FirewallRuleForGWRDSH
+        {
+            Direction = "Inbound"
+            Name = "Firewall-GW-RDSH-TCP-In"
+            DisplayName = "Firewall-GW-RDSH-TCP-In"
+            Description = "Inbound rule for CB to allow TCP traffic for configuring GW and RDSH machines during deployment."
+            DisplayGroup = "Connection Broker"
+            State = "Enabled"
+            Access = "Allow"
+            Protocol = "TCP"
+            LocalPort = "5985"
+            Ensure = "Present"
+        }
+        WindowsFeature RDS-RD-Server
+        {
+            Ensure = "Present"
+            Name = "RDS-RD-Server"
+        }
+    }
+}
