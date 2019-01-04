4f79f782cdd0d6e79437283f4cf2446cffcd67b3
viswanadham <sreenatha.kk@gmail.com>
2019-01-05 01:22:03 +0530

    added files

--- win10/Scripts/Script1.ps1
+++ win10/Scripts/Script1.ps1
@@ -5,15 +5,15 @@ $log = $PolicyList | Out-String
 Write-Output $log
 
 Invoke-WebRequest -Uri "https://github.com/viswanadhamkudapu/Repository/blob/master/win10/Scripts/DeployAgent.zip" -OutFile "C:\DeployAgent.zip"
-    Write-Log -Message "Downloaded DeployAgent.zip into this location C:\"
+    #Write-Log -Message "Downloaded DeployAgent.zip into this location C:\"
 
     #Creating a folder inside rdsh vm for extracting deployagent zip file
     New-Item -Path "C:\DeployAgent" -ItemType directory -Force -ErrorAction SilentlyContinue
-    Write-Log -Message "Created a new folder 'DeployAgent' inside VM"
+    #Write-Log -Message "Created a new folder 'DeployAgent' inside VM"
     Expand-Archive "C:\DeployAgent.zip" -DestinationPath "C:\DeployAgent" -ErrorAction SilentlyContinue
-    Write-Log -Message "Extracted the 'Deployagent.zip' file into 'C:\Deployagent' folder inside VM"
+    #Write-Log -Message "Extracted the 'Deployagent.zip' file into 'C:\Deployagent' folder inside VM"
     Set-Location "C:\DeployAgent"
-    Write-Log -Message "Setting up the location of Deployagent folder"
+    #Write-Log -Message "Setting up the location of Deployagent folder"
 
     .\Scripts\Script1.ps1 -FileURI "https://github.com/viswanadhamkudapu/Repository/blob/master/win10/Scripts/DeployAgent.zip" -registrationToken "" -ActivationKey "NJCF7-PW8QT-3324D-688JX-2YV66" -rdshIs1809OrLater 'True' -localAdminUserName "vmadmin" -localAdminPassword "keepcalm@123"
 
